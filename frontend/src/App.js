53af915a87526d3dfa8719251225579f81407916
RISHI GOLI <rg0001.ayconnects@gmail.com>
2024-08-19 09:15:55 +0530

    design part is done partially

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,63 @@
 import logo from './logo.svg';
 import './App.css';
 
+const gridItems = [
+  { title: 'Branches', description: 'Abstract Branches lets you manage, version, and document your designs in one place.' },
+  { title: 'Manage your account', description: 'Configure your account settings, such as your email, profile details, and password.' },
+  { title: 'Manage organizations, teams, and projects', description: 'Use Abstract organizations, teams, and projects to organize your people and your work.' },
+  { title: 'Manage billing', description: 'Change subscriptions and payment details.' },
+  { title: 'Authenticate to Abstract', description: 'Set up and configure SSO, SCIM, and Just-in-Time provisioning.' },
+  { title: 'Abstract support', description: 'Get in touch with a human.' }
+]
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container">
+      <header>
+        <span>Abstract | Help Center</span>
+        <button >Submit a request</button>
       </header>
+
+      <div className='banner-section'>
+        <h1>How can we help ?</h1>
+        <input type="text" placeholder='search' />
+      </div>
+
+      <div className='cards-section'>
+        <div className='grid-container'>
+          {
+            gridItems.map((item, index) => (
+              <div className='grid-item'>
+                <div className='grid-item-title'>
+                  Branches
+                </div>
+                <div className='grid-item-desciption'>
+                  asdf asf asd asdf asga gagasg  asg asg agag asgasgasgasgagag ag agasgasg asgasg as
+                </div>
+              </div>
+            ))
+          }
+        </div>
+      </div>
+
+      <footer>
+        <div className='footer-items-container'>
+          <h3>About</h3>
+          <h6>Branches</h6>
+        </div>
+        <div className='footer-items-container'>
+          <h3>Resources</h3>
+          <h6>Branches</h6>
+        </div>
+        <div className='footer-items-container'>
+          <h3>Community</h3>
+          <h6>Branches</h6>
+        </div>
+        <div className='footer-items-container'>
+          <h3>Company</h3>
+          <h6>Branches</h6>
+        </div>
+      </footer>
     </div>
   );
 }
